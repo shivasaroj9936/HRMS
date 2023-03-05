2ba8f23aa5913c64c4eba5f3379826a98a523eae
Shiva Saroj <shiva.saroj@appinventiv.com>
2023-03-05 22:09:43 +0530

    fixes

--- src/app/modules/features/modules/directory/directory.component.ts
+++ src/app/modules/features/modules/directory/directory.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormService } from 'src/app/services/form-service/form.service';
 import { DIRECTORY_EMPLOYEE_DATA, IDIRECTORY_DATA } from '../dashboard/interfaces/interfaces';
 import { BASIC_INFORMATION } from './constants/ui-text';
 
@@ -10,11 +12,49 @@ import { BASIC_INFORMATION } from './constants/ui-text';
 export class DirectoryComponent implements OnInit {
 
   uiText=BASIC_INFORMATION;
+  data:IDIRECTORY_DATA[]=DIRECTORY_EMPLOYEE_DATA;
   directoryDataList :IDIRECTORY_DATA[]=DIRECTORY_EMPLOYEE_DATA;
-  departments=['dadd','dddddd','reredere','dsfere']
-  constructor() { }
+  departments=['All','Angular','Node js','React Js','Android']
+  filterForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private _formService: FormService) {
+    
+   }
 
   ngOnInit(): void {
+    this.createForm();
   }
 
+  createForm() {
+    this.filterForm = this.formBuilder.group({
+      name: this._formService.getControl('name'),
+      technology:this._formService.getControl('name'),
+    });
+  }
+
+
+  getControl(control: string) {
+    return this.filterForm?.controls[control] as FormControl;
+  }
+ filter(){
+  this.getControl('name').patchValue(this.getControl('name').value.trim());
+  let tech =this.getControl('technology').value.toLowerCase();
+  let name =this.getControl('name').value.toLowerCase();
+  let newData = this.data.filter((item)=>{
+    if(tech=='all'){
+      return item.EMP_NAME.toLowerCase() == name
+    }else{
+      return (item.EMP_NAME.toLowerCase() == name && item.EMP_TECHNOLOGY.toLowerCase() == tech);
+
+    }
+  });
+  this.directoryDataList=newData;
+  console.log(newData);
+
+  
+ }
+ reset(){
+  this.directoryDataList =this.data;
+
+ }
 }
--- src/app/modules/features/modules/directory/directory.module.ts
+++ src/app/modules/features/modules/directory/directory.module.ts
@@ -8,6 +8,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ValidationErrorModule } from 'src/app/pipes/validation-error/validation-error.module';
 
 
 @NgModule({
@@ -21,7 +23,10 @@ import { MatIconModule } from '@angular/material/icon';
     MatInputModule,
     MatSelectModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    ReactiveFormsModule, 
+    FormsModule,
+    ValidationErrorModule
   ]
 })
 export class DirectoryModule { }
