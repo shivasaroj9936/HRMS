d78010fa377f4ca246d259d36eda66fbc79b17b1
Shiva Saroj <shiva.saroj@appinventiv.com>
2023-05-27 21:21:38 +0530

    loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,40 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { LoaderService } from './services/loader-service/loader.service';
+import { ChangeDetectorRef, Component, OnInit } from "@angular/core";
+import { LoaderService } from "./services/loader-service/loader.service";
+import { NavigationEnd, NavigationStart, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   showLoader = false;
 
-  title = 'HRMS';
-  constructor( private loaderService:LoaderService,private cdr:ChangeDetectorRef){
-
-  }
+  title = "HRMS";
+  constructor(
+    private loaderService: LoaderService,
+    private cdr: ChangeDetectorRef,
+    private router: Router
+  ) {}
   ngOnInit(): void {
-    this.loaderService.loading$.subscribe((data)=>{
-      setTimeout(()=>{
-        this.showLoader=false;
-      },0)
-    })
+    this.listenRouterEvents();
+    this.loaderService.loading$.subscribe((data) => {
+      setTimeout(() => {
+        this.showLoader = false;
+      }, 0);
+    });
+  }
+
+  listenRouterEvents() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.showLoader = true;
+      } else if (event instanceof NavigationEnd) {
+        setTimeout(() => {
+          this.showLoader = false;
+        }, 1000);
+      }
+    });
   }
   ngAfterContentChecked() {
     this.cdr.detectChanges();
